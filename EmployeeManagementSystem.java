5f3d0e6d44407732201510f45deac70e8c896dcc
khalid ibizzi <ibizzikhalid19@gmail.com>
2024-07-10 23:19:27 +0100

    added 4&6

--- EmployeeManagementSystem.java
+++ EmployeeManagementSystem.java
@@ -72,6 +72,7 @@ public class EmployeeManagementSystem {
 
         }
         
+        
 
     } 
 
@@ -86,7 +87,6 @@ public class EmployeeManagementSystem {
         }
 
         public static void addmanager(Manager manager){
-            
             managers.add(manager);
             System.out.println(manager.name+"has been added successfuly !");
         }
@@ -111,12 +111,67 @@ public class EmployeeManagementSystem {
             }
         }
         public static void removeManager(String nametoremove) {
-            managers.removeIf(manager -> manager.name.equals(nametoremove));
+            managers.removeIf(manager -> manager.name.equalsIgnoreCase(nametoremove));
+        }
+
+        public static void editManager(String name){
+            for (Manager manager : managers) {
+                if(manager.name.equalsIgnoreCase(name)){
+                    System.out.println("what do u wanna change ? (name/age/phone/...) ");
+                    Scanner sc = new Scanner(System.in);
+                    String choice = sc.nextLine();
+                    switch (choice){
+                        case "name":
+                            System.out.println("enter the new name : ");
+                            String newName =sc.nextLine();
+                            manager.name = newName;
+
+                        case "age":
+                            System.out.println("enter the new age : ");
+                            int newAge = sc.nextInt();
+                            manager.age = newAge;
+                        
+                        case "phone":
+                            System.out.println("enter the new phone : ");
+                            String newPhone = sc.nextLine();
+                            manager.phone = newPhone;
+                        case "salary":
+                            System.out.println("enter the new salary : ");
+                            double newSalary = sc.nextDouble();
+                            manager.baseSalary = newSalary;
+                        case "bonus":
+                            System.out.println("enter the new bonus : ");
+                            double newBonus = sc.nextDouble();
+                            manager.bonus = newBonus;
+                        case "address":
+                            System.out.println("enter the new address : ");
+                            String newAddress = sc.nextLine();
+                            manager.address = newAddress;
+                        case "email":
+                            System.out.println("enter the new email : ");
+                            String newEmail = sc.nextLine();
+                            manager.email = newEmail;
+
+                        }
+                }else{
+                    System.out.println("the name u gave isn't in managers list");
+                }
+            }
         }
 
 
+        
+        public static void showManager(String name){
+            for (Manager man : managers){
+                if(man.name.equalsIgnoreCase(name)){
+                    man.displayDetails();
+                }
+            }
+
+        }
     }
 
+    
     public static class Developer extends Employee{
         static List<Developer> Devs = new ArrayList<>(); 
         int overtimehours;
@@ -171,6 +226,62 @@ public class EmployeeManagementSystem {
         public static void removeDev(String nametoremove) {
             Devs.removeIf(dev -> dev.name.equals(nametoremove));
         }
+
+        public static void editDev(String name){
+            for (Developer dev : Devs) {
+                if(dev.name.equalsIgnoreCase(name)){
+                    System.out.println("what do u wanna change ? (name/age/phone/...) ");
+                    Scanner sc = new Scanner(System.in);
+                    String choice = sc.nextLine();
+                    switch (choice){
+                        case "name":
+                            System.out.println("enter the new name : ");
+                            String newName =sc.nextLine();
+                            dev.name = newName;
+                            break;
+
+                        case "age":
+                            System.out.println("enter the new age : ");
+                            int newAge = sc.nextInt();
+                            dev.age = newAge;
+                            break;
+
+                        case "phone":
+                            System.out.println("enter the new phone : ");
+                            String newPhone = sc.nextLine();
+                            dev.phone = newPhone;
+                            break;
+                        case "salary":
+                            System.out.println("enter the new salary : ");
+                            double newSalary = sc.nextDouble();
+                            dev.baseSalary = newSalary;
+                            break;
+
+                        case "address":
+                            System.out.println("enter the new address : ");
+                            String newAddress = sc.nextLine();
+                            dev.address = newAddress;
+                            break;    
+                        case "email":
+                            System.out.println("enter the new email : ");
+                            String newEmail = sc.nextLine();
+                            dev.email = newEmail;
+                            break;
+                        }
+                }else{
+                    System.out.println("the name u gave isn't in devs list");
+                }
+            }
+        }
+
+        public static void showDev(String name){
+            for (Developer dev : Devs){
+                if(dev.name.equalsIgnoreCase(name)){
+                    dev.displayDetails();
+                }
+            }
+
+        }
     }
     
     
@@ -202,26 +313,37 @@ public class EmployeeManagementSystem {
 
                     System.out.println("what's the Emp's name ? ");
                     String name = sc.nextLine();
+
                     System.out.println("what's their base salary ? ");
                     Double baseSalary = sc.nextDouble();
+                    
                     System.out.println("what's their age? ");
                     Integer age = sc.nextInt();
+                    
                     System.out.println("in what year did they join us ? ");
                     Integer joiningYear = sc.nextInt();
+                    
                     System.out.println("what's the Emp's Designation? ");
                     String designation = sc.nextLine();
+                    
                     System.out.println("what's their departement? ");
                     String department = sc.nextLine();
+                    
                     System.out.println("what's their address ? ");
                     String address = sc.nextLine();
+                    
                     System.out.println("what's their email ? ");
                     String email = sc.nextLine();
+                    
                     System.out.println("what's their Pone number ? ");
                     String phone = sc.nextLine();
+                    
                     System.out.println("what's their gender ? ");
                     String gender = sc.nextLine();
+                    
                     System.out.println("what's their maritalStatus ? ");
                     String maritalStatus = sc.nextLine();
+                    
                     System.out.println("how are they performing at work ? ");
                     String performance = sc.nextLine();
 
@@ -247,7 +369,7 @@ public class EmployeeManagementSystem {
                      break;
                 
                 case 2:
-                   
+                //     Display Details
                     System.out.println("1 - Manager details ?");
                     System.out.println("2 - Devs details ?");
                     System.out.println("3 - Exit ?");
@@ -274,11 +396,13 @@ public class EmployeeManagementSystem {
                     //remove an employee from the list by name
                     System.out.println("enter the name of the employee you want to remove : ");
                     String nametoremove = sc.nextLine();
+
                     System.out.println("are they a dev or a manager ?");
                     String typetoremove = sc.nextLine();
-                    if (typetoremove.equals("manager")){
+
+                    if (typetoremove.equalsIgnoreCase("manager")){
                         Manager.removeManager(nametoremove);
-                    }else if (typetoremove.equals("dev")){
+                    }else if (typetoremove.equalsIgnoreCase("dev")){
                         Developer.removeDev(nametoremove);
                     }else{
                         System.out.println("invalid job type ! but lets ignor it for now");
@@ -288,9 +412,41 @@ public class EmployeeManagementSystem {
                     break;
 
                 case 4:
+                // Modify An employee's Details
+                    System.out.println("enter the name of the employee you want to modify : ");
+                    String nametomodify = sc.nextLine();
+
+                    System.out.println("are they a dev or a manager ?");
+                    String typetomodify = sc.nextLine();
+
+                    if (typetomodify.equalsIgnoreCase("manager")){
+                        Manager.editManager(nametomodify);
+                    }else if (typetomodify.equalsIgnoreCase("dev")){
+                        Developer.editDev(nametomodify);
+                    }else{
+                        System.out.println("invalid job type ! but lets ignor it for now");
+                        }
+                    
                     break;
 
                 case 5:
+                // Display A Specefic Employee's Details
+                    System.out.println("whats the name of the employee");
+                    String nametoshow = sc.nextLine();
+                    System.out.println("are they a dev or a manager ?");
+                    String typetoshow = sc.nextLine();
+                    while(true){
+                    if ( typetoshow.equalsIgnoreCase("dev")){
+                        Developer.showDev(nametoshow);
+
+                        break;
+                    }else if ( typetoshow.equalsIgnoreCase("manager")){
+                        Manager.showManager(nametoshow);
+
+                        break;
+                    }else{
+                        System.out.println("not a valide type , dev or manager !?");
+                    }}
                     break;
                 case 6:
                     running = false;
